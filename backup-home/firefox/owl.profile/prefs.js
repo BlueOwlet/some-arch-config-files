024a1cec3ead094f58488ea974852887fb6fea3d
owl <kamiowlet@gmail.com>
2023-09-02 15:21:06 -0600

    auto backup: 09/02/2023 15:21:05

--- backup-home/firefox/owl.profile/prefs.js
+++ backup-home/firefox/owl.profile/prefs.js
@@ -195,7 +195,7 @@ user_pref("services.sync.engine.creditcards", true);
 user_pref("services.sync.engine.prefs.modified", false);
 user_pref("services.sync.forms.lastSync", "1693686167.59");
 user_pref("services.sync.forms.syncID", "5EbdlajMKVtn");
-user_pref("services.sync.globalScore", 9);
+user_pref("services.sync.globalScore", 10);
 user_pref("services.sync.lastPing", 1693616541);
 user_pref("services.sync.lastSync", "Sat Sep 02 2023 15:16:38 GMT-0600 (Central Standard Time)");
 user_pref("services.sync.lastTabFetch", 1693689398);
