acac545f646fc48281877a34865215b103d6f541
owl <kamiowlet@gmail.com>
2023-10-02 21:11:19 -0600

    auto backup: 10/02/2023 21:11:17

---
+++ backup_home/.config/Code - OSS/User/History/4b946fe1/vqhN.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+. manage
+
+
+list_functions(){
+
+    
+    local file=$1
+    echo "MENU FOR: $file"
+    awk_output=`awk -F '(' '/()\{/ {print $1}' $file`    
+
+    readarray -t funcs <<< "$awk_output"
+    counter=0
+    for func in ${funcs[@]}
+    do
+        ((counter++))
+        echo "$counter $func"
+    done
+    echo "Select Option (default: exit):"
+}
+
+select_option(){
+    local option
+    read option
+    echo $option
+}
+
+
+
+menu() {
+    local file=$1
+
+    while true
+    do
+        clear  
+        
+        list_functions $file
+        local option=`select_option`
+        
+
+        echo "RUNNING FUNCTION: $option"
+        if [ "$option" == "" ];then
+            main
+        fi
+        ${funcs[$option-1]}
+    done
+
+
+
+}
